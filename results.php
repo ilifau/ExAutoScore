c6e77f1b18771fb1bc538adcec42f88fc9ab823f
Fred Neumann <fred.neumann@fau.de>
2021-08-06 02:35:55 +0200

    write feedback_html to feedback.html file write received feedback files (not yet received)

--- results.php
+++ results.php
@@ -1,17 +1,16 @@
 <?php
 chdir(__DIR__ . '/../../../../../../../');
 
+//require_once ('./include/inc.debug.php');
+//log_request();
+
+
 // we need access handling
 include_once 'Services/Context/classes/class.ilContext.php';
 ilContext::init(ilContext::CONTEXT_RSS);
 
 require_once("Services/Init/classes/class.ilInitialisation.php");
 ilInitialisation::initILIAS();
-
-//require_once ('./include/inc.debug.php');
-//$request = $DIC->http()->request();
-//$content = $request->getBody()->getContents();
-//log_var($content, 'content');
 
 require_once (__DIR__ . '/classes/class.ilExAutoScoreConnector.php');
 $connector = new ilExAutoScoreConnector();
